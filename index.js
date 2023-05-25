cff988a7d1538c813b435e06dfd17c886388ca98
mjbalagtas <mjm.balagtas@gmail.com>
2023-05-25 11:24:58 +0800

    see changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Watchtower!</h1>')
+  res.send('<h1>watch again!</h1>')
 })
 
 const PORT = 8080
